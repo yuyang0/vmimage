6bf250cf3f9092933b0b25111d0218634594f4c7
YuYang <yyangplus@gmail.com>
2024-03-16 21:42:46 +0800

    check and refine config

--- types/config.go
+++ types/config.go
@@ -1,5 +1,11 @@
 package types
 
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+)
+
 type DockerConfig struct {
 	Endpoint string `toml:"endpoint" default:"unix:///var/run/docker.sock"`
 	Auth     string `toml:"-"` // in base64
@@ -19,4 +25,31 @@ type Config struct {
 	Type    string        `toml:"type" default:"docker"`
 	Docker  DockerConfig  `toml:"docker"`
 	Citadel CitadelConfig `toml:"citadel"`
+}
+
+func (cfg *Config) CheckAndRefine() error {
+	switch cfg.Type {
+	case "docker":
+		if cfg.Docker.Username == "" || cfg.Docker.Password == "" {
+			return errors.New("docker's username or password should not be empty")
+		}
+		auth := map[string]string{
+			"username": cfg.Docker.Username,
+			"password": cfg.Docker.Password,
+		}
+		authBytes, _ := json.Marshal(auth)
+		cfg.Docker.Auth = base64.StdEncoding.EncodeToString(authBytes)
+	case "citadel":
+		if cfg.Citadel.Username == "" || cfg.Citadel.Password == "" {
+			return errors.New("ImageHub's username or password should not be empty")
+		}
+		if cfg.Citadel.Addr == "" {
+			return errors.New("ImageHub's address shouldn't be empty")
+		}
+	case "mock":
+		return nil
+	default:
+		return errors.New("unknown image hub type")
+	}
+	return nil
 }
